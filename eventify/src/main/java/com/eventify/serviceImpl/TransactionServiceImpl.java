eef45c6f641cff9185343bc98c7686fcb4ab2e38
priyanshi porwal <74495039+priyanshi2024@users.noreply.github.com>
2023-12-17 07:16:33 -0800

    Add files via upload
    initial commit

---
+++ eventify/src/main/java/com/eventify/serviceImpl/TransactionServiceImpl.java
@@ -0,0 +1,15 @@
+package com.eventify.serviceImpl;
+
+import org.springframework.stereotype.Service;
+
+import com.eventify.service.TransactionService;
+
+@Service
+public class TransactionServiceImpl implements TransactionService{
+	
+	@Override
+	public String sendOtp(String phoneNumber) {
+		return null;
+	}
+
+}
